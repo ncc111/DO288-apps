3e0bcea5b762757fdead11cb35d71715c92d6a00
ncc111 <39723749+ncc111@users.noreply.github.com>
2024-10-03 14:24:19 +0800

    Create log.py

---
+++ html-helloworld/log.py
@@ -0,0 +1,4 @@
+with open('example.txt', 'w') as file:
+    file.write('1st line\n')  # Write the first line
+    file.write('2nd line\n')  # Write the second line
+    file.write('3rd line\n')  # Write the third line
